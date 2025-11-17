8115f602db2756d1beef58ee7f2b1711f4b24b82
VortexOwl <glacialisMustela@proton.me>
2025-11-17 08:25:48 +0300

    fix the project structure

---
+++ src/start.py
@@ -0,0 +1,9 @@
+# ----------------------------------------------------------------------------#
+# Project modules                                                             #
+# ----------------------------------------------------------------------------#
+import base_config_project
+from src.word_search import word_search
+
+
+def start() -> None:
+    word_search()
