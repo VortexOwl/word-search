b68cb50e9ededb7a5fc730dd56b6a8eadb6630d0
VortexOwl <glacialisMustela@proton.me>
2026-01-15 02:57:23 +0300

    Update: 1) Func word serch; 2) Logs. Fix: 1) README.md.

--- src/logs/logs.py
+++ src/logs/logs.py
@@ -7,11 +7,13 @@ from traceback import extract_stack
 # ----------------------------------------------------------------------------#
 # Project modules                                                             #
 # ----------------------------------------------------------------------------#
-from src.logs.config import is_file_info, is_file_debug, is_file_warning, is_file_error, is_file_fatal
+from src.logs.config import is_file_info, is_file_debug, is_file_warning, is_file_error, is_file_fatal, is_console_handler
 from src.logs.loggers import logger_main, logger_info, logger_debug, logger_warning, logger_error, logger_fatal
 from src.utilities.utilities import creating_necessary_folders, clearing_logs
 
 def log_info(message:str) -> None:
+    if not is_console_handler:
+        print(message)
     message: str = f'Module: {extract_stack(limit=2)[0].filename.split('/')[-1]} | Func: {extract_stack(limit=2)[0].name} | Line: {extract_stack(limit=2)[0].lineno} | Message: {message}'
     logger_main.info(msg=message)
     if is_file_info:
--- src/word_search.py
+++ src/word_search.py
@@ -2,6 +2,7 @@
 # Embedded libraries                                                          #
 # ----------------------------------------------------------------------------#
 from os import path, mkdir
+from re import sub as re_sub
 
 # ----------------------------------------------------------------------------#
 # Project modules                                                             #
@@ -10,88 +11,86 @@ from src.logs.logs import log_info
 from src.utilities.utilities import creating_necessary_folders
 
 
-def word_search() -> None:
-    # ----------------------------------------------------------------------------#
-    # Список всех русских букв                                                    #
-    # ----------------------------------------------------------------------------#
-    list_letters = [
+def get_quantity_letters() -> int:
+    log_info(message='Введите количество букв искомого слова.')
+    quantity_letters = re_sub(pattern=r'\D', repl='', string=input())
+    while quantity_letters == '':
+        log_info(message='Введено некорректное количество букв слова. Повторите ввод количества букв слова.')
+        quantity_letters = re_sub(pattern=r'\D', repl='', string=input())
+    log_info(message=f"Введено число: '{quantity_letters}'.")
+    return int(quantity_letters)
+
+
+def get_letters_not_in_word() -> str:
+    log_info(message='Введите буквы, которые отсутствуют в искомом слове.')
+    letters_not_in_word = re_sub(pattern=r'[^а-я-]' , repl='', string=''.join(dict.fromkeys(input().lower())))
+    log_info(message=f'Введены буквы: {list(letters_not_in_word)}.')
+    return letters_not_in_word
+
+
+def get_letters_in_word(letters_not_in_word:str) -> str:
+    log_info(message='Введите буквы, которые присутствуют в искомом слове.')
+    letters_in_word = re_sub(pattern=rf'[{letters_not_in_word}]', repl='', string=''.join(dict.fromkeys(input().lower())))
+    log_info(message=f'Введены буквы: {list(letters_in_word)}.')
+    return letters_in_word
+
+
+def get_letters_not_in_positions(letters_in_word:str, quantity_letters:int) -> list:
+    log_info(message='Введите буквы искомого слова, которые отсутствуют в определенных его позициях.')
+    letters_not_in_positions = list()
+    for index in range(quantity_letters):
+        log_info(message=f'Позиция {index+1} ({index*'+'}*{(quantity_letters-index-1)*'+'}):')
+        letters_not_in_positions_item = re_sub(pattern=rf'[^{letters_in_word}]' , repl='', string=''.join(dict.fromkeys(input().lower())))
+        log_info(message=f'Введены буквы: {list(letters_not_in_positions_item)}.')
+        letters_not_in_positions.append(letters_not_in_positions_item)
+    return letters_not_in_positions
+
+
+def get_letters_in_positions(letters_in_word:str, quantity_letters:int) -> str:
+    log_info(message='Введите буквы искомого слова, которые присутсвуют в определенных его позициях.')
+    letters_in_positions = list()
+    for index in range(quantity_letters):
+        log_info(message=f'Позиция {index+1} ({index*'+'}*{(quantity_letters-index-1)*'+'}):')
+        letters_in_positions_item = re_sub(pattern=rf'[^{letters_in_word}]*([{letters_in_word}])*.*', repl=r'\1', string=''.join(dict.fromkeys(input().lower())))
+        log_info(message=f'Введены буквы: {list(letters_in_positions_item)}.')
+        letters_in_positions.append(letters_in_positions_item)
+    return letters_in_positions
+
+
+def get_filters() -> tuple:
+    quantity_letters = get_quantity_letters()
+    letters_not_in_word = get_letters_not_in_word()
+    letters_in_word = get_letters_in_word(letters_not_in_word=letters_not_in_word)
+    letters_not_in_positions = get_letters_not_in_positions(letters_in_word=letters_in_word, quantity_letters=quantity_letters)
+    letters_in_positions = get_letters_in_positions(letters_in_word=letters_in_word, quantity_letters=quantity_letters)
+    return quantity_letters, set(letters_not_in_word), set(letters_in_word), letters_not_in_positions, letters_in_positions
+    
+
+def word_search():
+    quantity_letters, letters_not_in_word, letters_in_word, letters_not_in_positions, letters_in_positions = get_filters()
+    letters = {
         'й', 'ц', 'у', 'к', 'е', 'н', 'г', 'ш', 'щ', 'з', 'х', 'ъ', 'ф', 'ы', 'в', 
         'а', 'п', 'р', 'о', 'л', 'д', 'ж', 'э', 'я', 'ч', 'с', 'м', 'и', 'т', 'ь', 
-        'б', 'ю']
-
-    # ----------------------------------------------------------------------------#
-    # Получение русских слов из словаря с сохранением в список тех, что состоят   # 
-    # из нужного количества букв                                                  #
-    # ----------------------------------------------------------------------------#
+        'б', 'ю'}
     with open('resources/russian.txt', 'r', encoding='cp1251') as ru_words_file:
         russian_words = ru_words_file.read().lower()
-    list_words = russian_words.splitlines()
-
-    # ----------------------------------------------------------------------------#
-    # Определение количества букв слова.                                          #
-    # Пример: 5                                                                   #
-    # ----------------------------------------------------------------------------#
-    log_info(message='Введите количество букв искомого слова.')
-    quantity_letters_in_word = int(input())
-
-    # ----------------------------------------------------------------------------#
-    # Определение букв, которые есть в искомом слове.                             #
-    # Пример: ['а', 'р', 'д']                                                     #
-    # ----------------------------------------------------------------------------#
-    log_info(message='Введите подряд известные буквы искомого слова.')
-    input_data = list(input().replace(' ', '').lower())
-    letter_in_word = input_data
-
-    # ----------------------------------------------------------------------------#
-    # Определение букв, которые отсутствуют в слове и удаление их из списка букв. #
-    # Пример: ['б', 'н', 'к', 'х', 'о']                                           #
-    # ----------------------------------------------------------------------------#
-    log_info(message='Введите подряд буквы, которые отсутствуют в искомом слове.')
-    input_data = list(input().replace(' ', '').lower())
-    list_letters_not_in_word = input_data
-    list_letters = list(set(list_letters) - set(list_letters_not_in_word))
+    words = set(russian_words.splitlines())
     
-    # ----------------------------------------------------------------------------#
-    # Определение позиций букв, которые есть в слове.                             #
-    # Пример: ['+', '+', '+', 'д', 'а']                                           #
-    # ----------------------------------------------------------------------------#
-    log_info(message='Введите известные буквы по порядку так, как они находятся в искомом слове. Если буква неизвестна, замените её символом "+". Пример: "ба++я".')
-    input_data = list(input().replace(' ', '').lower())
-    list_found_letter = input_data
-
-    # ----------------------------------------------------------------------------#
-    # Определение позиций букв, на которых в слове их быть не должно.             #
-    # Пример: [[], [], [], [], []]                                                #
-    # ----------------------------------------------------------------------------#
-    input_data = list()
-    log_info(message='Введите подряд буквы которые отсутствуют в каждой из позиций искомого слова.')
-    for i in range(quantity_letters_in_word):
-        log_info(message='Позиция ' + str(i+1) + ':')
-        input_data.append(list(input().replace(' ', '').lower()))
-
-    list_not_letter = input_data
-
-    # ----------------------------------------------------------------------------#
-    # Поиск слов                                                                  #
-    # ----------------------------------------------------------------------------#
-    creating_necessary_folders(path='docs')
-    log_info(message='Начат перебор букв.')
-    with open('docs/words.txt', 'w') as words_file:
-        counter = 0
-        for word in list_words:
-            if len(word) == quantity_letters_in_word and all(letter in word for letter in letter_in_word) and not any(letter in word for letter in list_letters_not_in_word) and not word[0] == '-':
-                for i in range(quantity_letters_in_word): 
-                    if word[i] == list_found_letter[i] or (list_found_letter[i] == '+' and not any(letter in word[i] for letter in list_not_letter[i])):
-                        if i == quantity_letters_in_word - 1:
-                            if counter == 0:
-                                words_file.write(word)                                                                                                                         # Если проверяется последняя буква слова
-                            elif counter % 20 == 0:
-                                words_file.write('\n' + word)
-                            else:
-                                words_file.write(' ' + word)
-                            counter += 1
-                    else:
-                        break
-    log_info(message='Перебор букв завершен.')
+    words = [word for word in words if len(word) == quantity_letters and set(letters_in_word).issubset(word) and set(letters_not_in_word).isdisjoint(word)]
+    words = [word for word in words if not any((letter != letters_in_positions[index] or letter in letters_not_in_positions[index]) and letters_in_positions[index] != '' for index, letter in enumerate(word))]
+    quantity_words = len(words)
+    
+    log_info(message='Начат перебор слов.')
+    if quantity_words != 0:
+        creating_necessary_folders(path='docs')
+        with open('docs/words.txt', 'w') as words_file:
+            words_file.write(words[0] + ''.join('\n' + word if number % 20 == 0 else ' ' + word for number, word in enumerate(words[1:], start=1)))
+    
+    log_info(message='Перебор слов завершен.')
     log_info(message='Файл с отобранными словами создан по путю: "docs/words.txt".')
-    log_info(message='Найдено слов: ' + str(counter) + '.')
+    log_info(message=f'Найдено слов: {quantity_words}.')
+
+
+def start_word_search():
+    word_search()
+    
